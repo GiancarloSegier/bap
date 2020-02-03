21ade604270cd2879bcbbc0c3d0c9c2560f35fca
Laurens Segaert <laurenssegaert@Laurenss-MacBook-Pro.local>
2020-02-03 14:24:43 +0100

    race stats, invite empty state

---
+++ client/src/components/dashboard/Statistics.jsx
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import { inject, observer } from "mobx-react";
+
+import typoStyles from "../../styles/typo.module.css";
+import styles from "./Statistics.module.css";
+
+class Statistics extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { committees } = this.props.committeeStore;
+    console.log(committees);
+    return (
+      <div>
+        <h2 className={typoStyles.heading2}>Race statistics</h2>
+        <ul className={styles.counter}>
+          <li className={styles.stat}>
+            <img
+              src="http://placekitten.com/40/40"
+              className={styles.icon}
+              width="40"
+              height="40"
+              alt="Race committees"
+            ></img>
+            <div>
+              <div className={styles.number}>{committees.length}</div>
+              <p className={styles.description}>Race committees</p>
+            </div>
+          </li>
+          <li className={styles.stat}>
+            <img
+              src="http://placekitten.com/40/40"
+              className={styles.icon}
+              width="40"
+              height="40"
+              alt="Different countries"
+            ></img>
+            <div>
+              <div className={styles.number}>8</div>
+              <p className={styles.description}>Different countries</p>
+            </div>
+          </li>
+          <li className={styles.stat}>
+            <img
+              src="http://placekitten.com/40/40"
+              className={styles.icon}
+              width="40"
+              height="40"
+              alt="Pre-registred participants"
+            ></img>
+            <div>
+              <div className={styles.number}>200.000</div>
+              <p className={styles.description}>Pre-registred participants</p>
+            </div>
+          </li>
+          <li className={styles.stat}>
+            <img
+              src="http://placekitten.com/40/40"
+              className={styles.icon}
+              width="40"
+              height="40"
+              alt="Races on the same day"
+            ></img>
+            <div>
+              <div className={styles.number}>7</div>
+              <p className={styles.description}>Races on the same day</p>
+            </div>
+          </li>
+          <li className={styles.stat}>
+            <img
+              src="http://placekitten.com/40/40"
+              className={styles.icon}
+              width="40"
+              height="40"
+              alt="Pre-registred survivors"
+            ></img>
+            <div>
+              <div className={styles.number}>15.000</div>
+              <p className={styles.description}>Pre-registred survivors</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default inject(`committeeStore`)(observer(Statistics));
---
+++ client/src/components/dashboard/requests/RequestEmpty.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { inject, observer } from "mobx-react";
+
+import styles from "./Request.module.css";
+class RequestEmpty extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className={styles.emptyCard}>
+        <div className={styles.icon}>
+          <span className={styles.cross_line}></span>
+          <span className={styles.cross_line}></span>
+        </div>
+        <p>Create a new committee to invite their event managers</p>
+      </div>
+    );
+  }
+}
+
+export default inject(`committeeStore`)(observer(RequestEmpty));
