6e3446da5e9bb554e15cd95a67e43f2094ae481b
Laurens Segaert <laurenssegaert@Laurenss-MacBook-Pro.local>
2020-02-05 23:42:54 +0100

    dashboard member

---
+++ client/src/components/dashboard/announcements/AnnouncementSmall.jsx
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import styles from "./Announcement.module.css";
+import FontAwesome from "react-fontawesome";
+
+class AnnouncementSmall extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.getDate();
+  }
+
+  getDate() {
+    const requestDate = new Date(this.props.announcement.updatedAt);
+    const day = requestDate.getDate();
+    const month = requestDate.getMonth() + 1;
+    const year = requestDate.getFullYear();
+
+    const dateString = `${day < 10 ? "0" + day : day}-${
+      month < 10 ? "0" + month : month
+    }-${year}`;
+
+    this.setState({ dateString: dateString });
+  }
+  onView = () => {
+    this.props.onView();
+  };
+  onEdit = () => {
+    this.props.onEdit();
+  };
+
+  render() {
+    const { announcement, privileges } = this.props;
+
+    return (
+      <div className={styles.masonryBlock}>
+        <article className={styles.card}>
+          <div className={styles.container}>
+            <div className={styles.gridHeader2}>
+              <h2 className={styles.title}>{announcement.title}</h2>
+              <div className={styles.icons_top}>
+                {announcement.images.length > 0 ? (
+                  <FontAwesome className={styles.icon} name="photo" />
+                ) : null}
+                {announcement.images.length > 0 ? (
+                  <FontAwesome className={styles.icon} name="attachement" />
+                ) : null}
+              </div>
+            </div>
+            <p className={styles.date}>{this.state.dateString}</p>
+            <p className={styles.content}>{announcement.content}</p>
+            <button
+              type="button"
+              className={styles.readmore}
+              onClick={this.onView}
+            >
+              read more
+            </button>
+            {/* {announcement.images.length > 0 ? (
+              
+            ) : null} */}
+            {/* {announcement.attachment && announcement.attachment.length > 0 ? (
+              <a className={styles.icon_button} href={announcement.attachment}>
+                <FontAwesome
+                  name="paperclip"
+                  className={styles.paperclip_icon}
+                />
+              </a>
+            ) : null} */}
+          </div>
+        </article>
+      </div>
+    );
+  }
+}
+export default AnnouncementSmall;
