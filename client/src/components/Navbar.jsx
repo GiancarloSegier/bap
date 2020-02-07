d573c2efd3ffd62d9c059224828874a35941b335
Laurens Segaert <laurenssegaert@Laurenss-MacBook-Pro.local>
2020-02-07 11:46:34 +0100

    landing

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,80 +1,107 @@
-import React from "react";
+import React, { Component } from "react";
 import styles from "./Navbar.module.css";
 import { NavLink } from "react-router-dom";
 import { ROUTES } from "../constants";
 import { inject, observer } from "mobx-react";
 import AvatarNav from "./dashboard/AvatarNav";
 
-const Navbar = ({ userStore }) => {
-  return (
-    <ul className={styles.navbar}>
-      <li className={styles.logo}>
-        <NavLink exact={true} className={styles.navLink} to={ROUTES.home}>
-          RFTC
-        </NavLink>
-      </li>
-      <div className={styles.rightMenu}>
-        {userStore.authUser ? (
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount = () => {
+    window.addEventListener("scroll", this.getPageInfo);
+  };
+
+  getPageInfo = () => {
+    console.log(window.pageYOffset);
+    if (window.pageYOffset > 50) {
+      this.setState({ navBackground: true });
+    } else {
+      this.setState({ navBackground: false });
+    }
+  };
+
+  render() {
+    const { userStore } = this.props;
+    const { navBackground } = this.state;
+    return (
+      <ul
+        className={
+          styles.navbar +
+          " " +
+          (navBackground ? null : styles.background_hidden)
+        }
+      >
+        <li className={styles.logo}>
+          <NavLink exact={true} className={styles.navLink} to={ROUTES.home}>
+            RFTC
+          </NavLink>
+        </li>
+        <div className={styles.rightMenu}>
+          {userStore.authUser ? (
+            <li>
+              <NavLink
+                strict={true}
+                className={styles.navLink}
+                to={ROUTES.dashboard}
+                activeClassName={styles.active}
+              >
+                Dashboard
+              </NavLink>
+            </li>
+          ) : null}
           <li>
             <NavLink
               strict={true}
               className={styles.navLink}
-              to={ROUTES.dashboard}
+              to={ROUTES.designstudio}
               activeClassName={styles.active}
             >
-              Dashboard
+              Designstudio
             </NavLink>
           </li>
-        ) : null}
-        <li>
-          <NavLink
-            strict={true}
-            className={styles.navLink}
-            to={ROUTES.designstudio}
-            activeClassName={styles.active}
-          >
-            Designstudio
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            strict={true}
-            className={styles.navLink}
-            to={ROUTES.toolbox}
-            activeClassName={styles.active}
-          >
-            Toolbox
-          </NavLink>
-        </li>
-        {!userStore.authUser ? (
-          <>
-            <li>
-              <NavLink
-                strict={true}
-                className={styles.navLink + " " + styles.purpleLink}
-                to={ROUTES.request}
-                // activeClassName={styles.activeDark}
-              >
-                Request acces
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                strict={true}
-                className={styles.navLink + " " + styles.pinkLink}
-                to={ROUTES.login}
-                // activeClassName={styles.activeDark}
-              >
-                Member login
-              </NavLink>
-            </li>
-          </>
-        ) : (
-          <AvatarNav currentUser={userStore.authUser} />
-        )}
-      </div>
-    </ul>
-  );
-};
-
+          <li>
+            <NavLink
+              strict={true}
+              className={styles.navLink}
+              to={ROUTES.toolbox}
+              activeClassName={styles.active}
+            >
+              Toolbox
+            </NavLink>
+          </li>
+          {!userStore.authUser ? (
+            <>
+              <li>
+                <NavLink
+                  strict={true}
+                  className={styles.navLink + " " + styles.purpleLink}
+                  to={ROUTES.request}
+                  // activeClassName={styles.activeDark}
+                >
+                  Request acces
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  strict={true}
+                  className={styles.navLink + " " + styles.pinkLink}
+                  to={ROUTES.login}
+                  // activeClassName={styles.activeDark}
+                >
+                  Member login
+                </NavLink>
+              </li>
+            </>
+          ) : (
+            <AvatarNav currentUser={userStore.authUser} />
+          )}
+        </div>
+      </ul>
+    );
+  }
+}
 export default inject(`userStore`)(observer(Navbar));
